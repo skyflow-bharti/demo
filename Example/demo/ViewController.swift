bf1f44b98add28cea9cacd0abd80d1ef045da080
bharti-sagar <bharti.sagar@skyflow.com>
2023-03-06 20:22:08 +0530

    Initial commit with cocoapod lib template

---
+++ Example/demo/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  demo
+//
+//  Created by 118584001 on 03/06/2023.
+//  Copyright (c) 2023 118584001. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
